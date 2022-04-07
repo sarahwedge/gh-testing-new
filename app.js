380885bbfcace6e3a595f1a50b61ee20adc9393e
sarahwedge <sarah.wedge@ontariotechu.net>
2022-04-07 03:55:10 -0400

    Test

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('Hello World');
+const express = require('express');
+const app = express();
+
+app.get('/users/test', (req, res) => {
+    res.send({ msg: 'Testing' });
+});
+
+app.listen(5000, () => console.log('Running on Port 5000'));
